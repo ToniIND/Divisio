3ba8d37a3b9d728195f9047324d2d96050d317c8
ism49322073 <ism49322073@a03.informatica.escoladeltreball.org>
2018-01-24 12:12:46 +0100

    comentario

---
+++ divisor_divident.py
@@ -0,0 +1,12 @@
+#coding: utf8
+
+divisor=input("Indiqui el divisor: ")
+
+divident=input("Indiqui el divident: ")
+
+residuo=divisor%divident
+
+if residuo==0:
+	print "correcte"
+else:
+	print "incorrecte"
